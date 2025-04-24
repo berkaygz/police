154fe2c75e3061713faddb674aeeb5f4bfe69a74
berkaygz <berkaygoz@mail.com>
2025-04-24 00:58:19 +0000

    init

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// Copyright 2017-2025 @polkadot/extension authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import baseConfig from '@polkadot/dev/config/eslint';
+
+export default [
+  ...baseConfig,
+  {
+    rules: {
+      'import/extensions': 'off'
+    }
+  }
+];
---
+++ packages/extension-ui/src/Popup/AuthManagement/AccountManagement.tsx
@@ -0,0 +1,95 @@
+// Copyright 2019-2025 @polkadot/extension-ui authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import React, { useCallback, useContext, useEffect } from 'react';
+import { useParams } from 'react-router';
+
+import { AccountContext, ActionContext, Button } from '../../components/index.js';
+import { useTranslation } from '../../hooks/index.js';
+import { getAuthList, updateAuthorization } from '../../messaging.js';
+import { AccountSelection, Header } from '../../partials/index.js';
+import { styled } from '../../styled.js';
+
+interface Props {
+  className?: string;
+}
+
+function AccountManagement ({ className }: Props): React.ReactElement<Props> {
+  const { url } = useParams<{url: string}>();
+  const { selectedAccounts = [], setSelectedAccounts } = useContext(AccountContext);
+  const { t } = useTranslation();
+  const onAction = useContext(ActionContext);
+
+  useEffect(() => {
+    getAuthList()
+      .then(({ list }) => {
+        if (!list[url]) {
+          return;
+        }
+
+        setSelectedAccounts && setSelectedAccounts(list[url].authorizedAccounts);
+      })
+      .catch(console.error);
+  }, [setSelectedAccounts, url]);
+
+  const _onApprove = useCallback(
+    (): void => {
+      updateAuthorization(selectedAccounts, url)
+        .then(() => onAction('../index.js'))
+        .catch(console.error);
+    },
+    [onAction, selectedAccounts, url]
+  );
+
+  return (
+    <div className={`${className}`}>
+      <Header
+        showBackArrow
+        smallMargin={true}
+        text={t('Accounts connected to {{url}}', { replace: { url } })}
+      />
+      <div className='content'>
+        <AccountSelection
+          origin={url}
+          showHidden={true}
+          url={url}
+          withWarning={false}
+        />
+        <div className='footer'>
+          <Button
+            className='acceptButton'
+            onClick={_onApprove}
+          >
+            {t('Connect {{total}} account(s)', { replace: {
+              total: selectedAccounts.length
+            } })}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default styled(AccountManagement)<Props>`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  min-height: 550px;
+
+  .content {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
+  }
+
+  .footer {
+    background: var(--background);
+    flex-shrink: 0;
+  }
+
+  .acceptButton {
+    width: 90%;
+    margin: 0.5rem auto;
+  }
+`;
